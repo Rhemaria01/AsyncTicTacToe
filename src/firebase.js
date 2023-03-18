458059bfe69bef917ab5e23173c02115cca73cfd
Reenav Hemaria <git@SJCEM23>
2023-03-18 21:01:50 +0530

    security fix

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCTnqagCgEkCmq8hUiJm-0srq9kGadepPc",
-    authDomain: "asycn-tictactoe.firebaseapp.com",
-    projectId: "asycn-tictactoe",
-    storageBucket: "asycn-tictactoe.appspot.com",
-    messagingSenderId: "11255054321",
-    appId: "1:11255054321:web:370678def0b1df59410e50"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+}
+
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
     export default db;
